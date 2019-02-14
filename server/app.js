d8afeea37c92f16b521005a0c0add7d2e801dad8
Coty Beasley <coty@cotybeasley.name>
2019-02-14 14:27:15 -0800

    Added initial redirection service

---
+++ server/app.js
@@ -0,0 +1,22 @@
+// Vars
+const express = require('express');
+const app = express();
+const port = 80;
+
+// Index
+app.get('/', function (req, res) {
+    res.send('This is Coty Beasley\'s url redirection service.');
+});
+
+/* Ireland photo book
+   Format: year-trip-asset-number
+   Example: 2016-ireland-video-0
+ */
+app.get('/2016-ireland-video-0', function (req, res) {
+    res.redirect(302, 'https://www.youtube.com/watch?v=3JHUJHDRcHM');
+});
+
+// Port listening text
+app.listen(port, function () {
+    console.log('Example app listening on port ' + port + '!');
+});
